04eb0cf94212d62222a7863dea0c0644817de933
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-03-10 17:29:04 +0530

    Authatiction with clerk

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+ import { ClerkProvider } from '@clerk/clerk-react';
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
